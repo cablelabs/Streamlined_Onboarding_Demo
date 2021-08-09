970b9fd17ef8683ca856cd9b9ee27268dcd238b0
Andy Dolan <20274393+adolan5@users.noreply.github.com>
2021-08-09 11:09:46 -0600

    Reorganize main Pi UI

--- ocf_devices/slined_onboarding/gui/so_pi.py
+++ ocf_devices/slined_onboarding/gui/so_pi.py
@@ -6,59 +6,15 @@ from slined_onboarding import get_dpp_uri
 
 logger = logging.getLogger(__name__)
 
-try:
-    import RPi.GPIO as GPIO
-except:
-    logger.warn('Failed to import RPi GPIO, falling back to mock GPIO')
-    import Mock.GPIO as GPIO
-
-class Ui_MainWindow():
-    def __init__(self, MainWindow, iface_name):
+class SoPiUi(QtWidgets.QMainWindow):
+    def __init__(self, iface_name):
+        super().__init__()
         self.iface_name = iface_name
         self.qr_img = None
-        # self.qr_img = ImageQt(qrcode.make(raw_uri))
-        self._setupUi(MainWindow)
+        self.qr_code_shown = False
+        self._setupUi()
 
-    def _set_widgets(self):
-        self.centralwidget.setObjectName("centralwidget")
-        self.centralwidget.setCursor(QtCore.Qt.BlankCursor)
-
-        self.qr_widget = QtWidgets.QWidget(self.centralwidget)
-        self.qr_widget.setGeometry(QtCore.QRect(5, 5, 210, 210))
-        self.qr_widget.setObjectName("qr_widget")
-
-        self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.verticalLayoutWidget.setGeometry(QtCore.QRect(220, 10, 95, 221))
-        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
-
-    def _set_buttons(self):
-        self.button_layout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
-        self.button_layout.setContentsMargins(0, 0, 0, 0)
-        self.button_layout.setObjectName("button_layout")
-        self.qr_button = QtWidgets.QPushButton(self.verticalLayoutWidget)
-        self.qr_button.setObjectName("qr_button")
-        self.button_layout.addWidget(self.qr_button)
-        self.reset_button = QtWidgets.QPushButton(self.verticalLayoutWidget)
-        self.reset_button.setObjectName("reset_button")
-        self.button_layout.addWidget(self.reset_button)
-        self.toggle_button = QtWidgets.QPushButton(self.verticalLayoutWidget)
-        self.toggle_button.setObjectName("toggle_button")
-        self.button_layout.addWidget(self.toggle_button)
-        self.reboot_button = QtWidgets.QPushButton(self.verticalLayoutWidget)
-        self.reboot_button.setObjectName("reboot_button")
-        self.button_layout.addWidget(self.reboot_button)
-        self.qr_button.clicked.connect(self._toggle_qr_code)
-
-    def _setup_qr_code(self):
-        self.qr_code = QtWidgets.QLabel(self.qr_widget)
-        self.qr_code.setGeometry(QtCore.QRect(5, 5, 210, 210))
-        self.qr_code.setText("")
-        # self.qr_code.setPixmap(QtGui.QPixmap.fromImage(self.qr_img))
-        self.qr_code.setScaledContents(True)
-        self.qr_code.setObjectName("qr_code")
-        self.qr_code.hide()
-
-    def _toggle_qr_code(self):
+    def toggle_qr_code(self):
         if self.qr_img is None:
             try:
                 dpp_uri = get_dpp_uri(self.iface_name)
@@ -76,23 +32,63 @@ class Ui_MainWindow():
 
         self.qr_code_shown = not self.qr_code_shown
 
-    def _setupUi(self, MainWindow):
-        self.qr_code_shown = False
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(320, 240)
-        MainWindow.setToolButtonStyle(QtCore.Qt.ToolButtonIconOnly)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
+    def _set_widgets(self):
+        self.centralwidget = QtWidgets.QWidget(self)
+        self.centralwidget.setObjectName("centralwidget")
+        self.centralwidget.setCursor(QtCore.Qt.BlankCursor)
+        self.main_hz_layout = QtWidgets.QHBoxLayout(self.centralwidget)
+
+        self.qr_widget = QtWidgets.QWidget()
+        self.qr_widget.setGeometry(QtCore.QRect(10, 10, 200, 200))
+        self.qr_widget.setObjectName("qr_widget")
+        layout.addWidget(self.qr_widget)
+
+        self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        # self.verticalLayoutWidget.setGeometry(QtCore.QRect(220, 10, 95, 221))
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        layout.addWidget(self.verticalLayoutWidget)
+
+    def _set_buttons(self):
+        self.button_layout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.button_layout.setContentsMargins(0, 0, 0, 0)
+        self.button_layout.setObjectName("button_layout")
+        self.qr_button = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.qr_button.setObjectName("qr_button")
+        self.button_layout.addWidget(self.qr_button)
+        self.reset_button = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.reset_button.setObjectName("reset_button")
+        self.button_layout.addWidget(self.reset_button)
+        self.toggle_button = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.toggle_button.setObjectName("toggle_button")
+        self.button_layout.addWidget(self.toggle_button)
+        self.reboot_button = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.reboot_button.setObjectName("reboot_button")
+        self.button_layout.addWidget(self.reboot_button)
+        self.qr_button.clicked.connect(self.toggle_qr_code)
+        self.reboot_button.clicked.connect(self.close)
+
+    def _setup_qr_code(self):
+        self.qr_code = QtWidgets.QLabel(self.qr_widget)
+        self.qr_code.setGeometry(QtCore.QRect(5, 5, 210, 210))
+        self.qr_code.setScaledContents(True)
+        self.qr_code.setObjectName("qr_code")
+        self.qr_code.hide()
+
+    def _setupUi(self):
+        self.setObjectName("MainWindow")
+        self.resize(320, 240)
+        self.setToolButtonStyle(QtCore.Qt.ToolButtonIconOnly)
         self._set_widgets()
         self._set_buttons()
         self._setup_qr_code()
-        MainWindow.setCentralWidget(self.centralwidget)
+        self.setCentralWidget(self.centralwidget)
 
-        self._retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self._retranslateUi()
+        QtCore.QMetaObject.connectSlotsByName(self)
 
-    def _retranslateUi(self, MainWindow):
+    def _retranslateUi(self):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.qr_button.setText(_translate("MainWindow", "QR Code"))
         self.reset_button.setText(_translate("MainWindow", "RESET"))
         self.toggle_button.setText(_translate("MainWindow", "Toggle"))
