b98496002eb1987dd1f50e1d82c5ccfe8af746ac
Andy Dolan <20274393+adolan5@users.noreply.github.com>
2021-08-11 11:49:50 -0600

    Start abstracting high-level Lite calls to lib

---
+++ ocf_devices/lightswitch/soswitch.c
@@ -0,0 +1,85 @@
+#include "oc_api.h"
+#include "port/oc_clock.h"
+#include <stdio.h>
+#include <pthread.h>
+#include <signal.h>
+
+static int quit = 0;
+static pthread_mutex_t mutex;
+static pthread_cond_t cv;
+static struct timespec ts;
+
+static int
+app_init(void)
+{
+  int ret = oc_init_platform("Raspberry Pi", NULL, NULL);
+  ret |= oc_add_device("/oic/d", "oic.d.switch", "SO Light Switch", "ocf.1.0.0",
+                       "ocf.res.1.0.0", NULL, NULL);
+  return ret;
+}
+
+static void
+signal_event_loop(void)
+{
+  pthread_mutex_lock(&mutex);
+  pthread_cond_signal(&cv);
+  pthread_mutex_unlock(&mutex);
+}
+
+static void
+issue_requests(void)
+{
+  printf("TODO: Discovery request happens here\n");
+  // oc_do_ip_discovery("core.light", &discovery, NULL);
+}
+
+static void
+handle_signal(int signal)
+{
+  (void)signal;
+  printf("handle signal called\n");
+  signal_event_loop();
+  quit = 1;
+}
+
+int
+so_switch_init(char *storage_path)
+{
+  int init;
+  struct sigaction sa;
+  sigfillset(&sa.sa_mask);
+  sa.sa_flags = 0;
+  sa.sa_handler = handle_signal;
+  sigaction(SIGINT, &sa, NULL);
+
+  static const oc_handler_t handler = { .init = app_init,
+    .signal_event_loop = signal_event_loop,
+    .requests_entry = issue_requests };
+
+  printf("Calling storage config with path %s\n", storage_path);
+  oc_storage_config(storage_path);
+  init = oc_main_init(&handler);
+  if (init < 0)
+    return init;
+  return 0;
+}
+
+int
+so_switch_main_loop(void)
+{
+  oc_clock_time_t next_event;
+  while (quit != 1) {
+    next_event = oc_main_poll();
+    pthread_mutex_lock(&mutex);
+    if (next_event == 0) {
+      pthread_cond_wait(&cv, &mutex);
+    } else {
+      ts.tv_sec = (next_event / OC_CLOCK_SECOND);
+      ts.tv_nsec = (next_event % OC_CLOCK_SECOND) * 1.e09 / OC_CLOCK_SECOND;
+      pthread_cond_timedwait(&cv, &mutex, &ts);
+    }
+    pthread_mutex_unlock(&mutex);
+  }
+  oc_main_shutdown();
+  return 0;
+}
